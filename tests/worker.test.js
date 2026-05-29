08410805883818278bd0071f7c190580e4aad19e
TheAsianFish <jmchung2006@gmail.com>
2026-05-29 13:41:31 -0700

    tests

--- tests/worker.test.js
+++ tests/worker.test.js
@@ -1,7 +1,7 @@
 //test worker.js
 
 import { jest } from '@jest/globals';
-import worker from '../js/worker.js';
+import worker from '../src/js/worker.js';
 
 // Mock the environment variables
 const env = {
